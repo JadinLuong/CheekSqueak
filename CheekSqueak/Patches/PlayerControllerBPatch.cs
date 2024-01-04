5da11a656dfbff4e165d7b391246d9578a5df74a
Jadin Luong <jadin.luong@gmail.com>
2024-01-04 04:23:09 -0500

    Added files for the CheekSqueak mod.

---
+++ CheekSqueak/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CheekSqueak.Patches
+{
+
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        private static ManualLogSource cheekSqueakLog = CheekSqueakMod.Log;
+
+        public static FartAction fartAction;
+
+        public static float timer = 4.0f;
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void initializePlayerWhoopie(PlayerControllerB __instance)
+        {
+            if (NetworkManager.Singleton.LocalClientId == __instance.playerClientId)
+            {
+                cheekSqueakLog.LogInfo("Initializing");
+
+                fartAction = new FartAction();
+
+                Debug.Log(__instance.playerUsername);
+
+                fartAction.Start(__instance);
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void PlayerControllerB_Update(PlayerControllerB __instance)
+        {
+            if (!fartAction.player.isPlayerDead)
+            {
+                bool keyPressed = Keyboard.current[CheekSqueakMod.fartKey.Value].wasPressedThisFrame;
+
+                if (timer > 0.0f)
+                {
+                    timer -= Time.deltaTime;
+                }
+
+                if (keyPressed == true && timer < 0.0f)
+                {
+                    Debug.Log(fartAction.player.playerUsername + " farted");
+                    fartAction.PlayFartSound();
+                    timer = 4.0f;
+                }
+            }
+        }
+    }
+
+    public class FartAction
+    {
+
+        public WhoopieCushionItem whoopieCushionObject;
+
+        public PlayerControllerB player;
+
+        public void Start(PlayerControllerB playerInstance)
+        {
+            player = playerInstance;
+            whoopieCushionObject = Resources.FindObjectsOfTypeAll<WhoopieCushionItem>()[0];
+            SoundManager.Instance.tempAudio1 = player.itemAudio;
+            SoundManager.Instance.syncedAudioClips = whoopieCushionObject.fartAudios;
+        }
+        public void PlayFartSound()
+        {
+            SoundManager.Instance.tempAudio1.pitch = Random.Range(0.5f, 2.0f);
+            SoundManager.Instance.PlayAudio1AtPositionServerRpc(player.transform.position, UnityEngine.Random.Range(0, SoundManager.Instance.syncedAudioClips.Length));
+        }
+    }
+}
---
+++ CheekSqueak/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CheekSqueak.Patches;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace CheekSqueak
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class CheekSqueakMod : BaseUnityPlugin
+    {
+        private const string modGUID = "CheekSqueak.LCMod";
+        private const string modName = "Cheek Squeak";
+        private const string modVersion = "1.0.0.0";
+
+        public static ConfigEntry<Key> fartKey;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static CheekSqueakMod Instance;
+
+        public static ManualLogSource Log;
+
+        public static Key defaultKey = Key.F;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            Log = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            fartKey = Config.Bind("Bind", "fart", defaultKey, "");
+
+            Log.LogInfo("Binded F key to fart");
+
+            harmony.PatchAll(typeof(CheekSqueakMod));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+
+            Log.LogInfo("Cheek Squeak ready!");
+        }
+
+    }
+
+}
